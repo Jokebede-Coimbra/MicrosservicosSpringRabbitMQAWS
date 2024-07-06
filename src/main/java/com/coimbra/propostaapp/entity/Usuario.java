cae7be12c6c47721066e2ba6c6e4763140bdc756
Coimbra <adsjkbd@gmail.com>
2024-07-06 14:24:23 -0300

    Mapeando as entidades

---
+++ src/main/java/com/coimbra/propostaapp/entity/Usuario.java
@@ -0,0 +1,17 @@
+package com.coimbra.propostaapp.entity;
+
+import jakarta.persistence.*;
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String telefone;
+    private String renda;
+    @OneToOne(mappedBy = "usuario")
+    private Proposta proposta;
+}
