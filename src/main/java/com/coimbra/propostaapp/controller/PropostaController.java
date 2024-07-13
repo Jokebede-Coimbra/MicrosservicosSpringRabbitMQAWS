2e2324a9b43988c191fb86b2d07fbc270e7fd30a
Coimbra <adsjkbd@gmail.com>
2024-07-13 07:25:09 -0300

    Criando a classe service

--- src/main/java/com/coimbra/propostaapp/controller/PropostaController.java
+++ src/main/java/com/coimbra/propostaapp/controller/PropostaController.java
@@ -3,12 +3,15 @@ package com.coimbra.propostaapp.controller;
 import com.coimbra.propostaapp.dto.PropostaRequestDto;
 import com.coimbra.propostaapp.dto.PropostaResponseDto;
 import com.coimbra.propostaapp.service.PropostaService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/proposta")
 public class PropostaController {
