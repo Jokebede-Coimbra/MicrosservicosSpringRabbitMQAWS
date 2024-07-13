cb3d0119ed58fad926d11680ac5fa1d93bca9027
Coimbra <adsjkbd@gmail.com>
2024-07-13 07:04:30 -0300

    Criando a classe service

--- src/main/java/com/coimbra/propostaapp/controller/PropostaController.java
+++ src/main/java/com/coimbra/propostaapp/controller/PropostaController.java
@@ -2,6 +2,7 @@ package com.coimbra.propostaapp.controller;
 
 import com.coimbra.propostaapp.dto.PropostaRequestDto;
 import com.coimbra.propostaapp.dto.PropostaResponseDto;
+import com.coimbra.propostaapp.service.PropostaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/proposta")
 public class PropostaController {
 
+    private PropostaService propostaService;
+
     @PostMapping
     public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto requestDto) {
-        return null;
+        PropostaResponseDto response = propostaService.criar(requestDto);
+        return ResponseEntity.ok(response);
     }
 }
---
+++ src/main/java/com/coimbra/propostaapp/service/PropostaService.java
@@ -0,0 +1,13 @@
+package com.coimbra.propostaapp.service;
+
+import com.coimbra.propostaapp.dto.PropostaRequestDto;
+import com.coimbra.propostaapp.dto.PropostaResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropostaService {
+
+    public PropostaResponseDto criar(PropostaRequestDto resquestDto) {
+        return null;
+    }
+}
