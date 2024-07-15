6d4245ae24fceea31412928490cded8abb595565
Coimbra <adsjkbd@gmail.com>
2024-07-15 19:19:02 -0300

    Fazendo o mapeamendo da classe de usuário

--- src/main/java/com/coimbra/propostaapp/entity/Proposta.java
+++ src/main/java/com/coimbra/propostaapp/entity/Proposta.java
@@ -1,7 +1,22 @@
 package com.coimbra.propostaapp.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "tb_proposta")
 public class Proposta {
--- src/main/java/com/coimbra/propostaapp/mapper/PropostaMapper.java
+++ src/main/java/com/coimbra/propostaapp/mapper/PropostaMapper.java
@@ -3,10 +3,17 @@ package com.coimbra.propostaapp.mapper;
 import com.coimbra.propostaapp.dto.PropostaRequestDto;
 import com.coimbra.propostaapp.entity.Proposta;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 
 @Mapper
 public interface PropostaMapper {
 
+    @Mapping(target = "usuario.nome", source = "nome")
+    @Mapping(target = "usuario.sobrenome", source = "sobrenome")
+    @Mapping(target = "usuario.cpf", source = "cpf")
+    @Mapping(target = "usuario.telefone", source = "telefone")
+    @Mapping(target = "usuario.renda", source = "renda")
     Proposta convertDtoProposta(PropostaRequestDto propostaRequestDto);
 
 }
