7138d096c73c9f516fc4f9ce76ffa4f558c2c636
Coimbra <adsjkbd@gmail.com>
2024-07-16 06:01:34 -0300

    Otimizando ocódigo com o CASCADE

--- src/main/java/com/coimbra/propostaapp/entity/Proposta.java
+++ src/main/java/com/coimbra/propostaapp/entity/Proposta.java
@@ -1,13 +1,6 @@
 package com.coimbra.propostaapp.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,7 +23,8 @@ public class Proposta {
     private Boolean aprovado;
     private boolean integrada;
     private String observacao;
-    @OneToOne
+
+    @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "id_usuario")
     private Usuario usuario;
 }
